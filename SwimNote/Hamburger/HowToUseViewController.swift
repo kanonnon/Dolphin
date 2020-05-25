846b15d1d4d5b26879463e6501225f988bab5447
kanonnnon <amechan527@gmail.com>
2020-05-25 16:26:51 +0900

    2020/5/25

---
+++ SwimNote/Hamburger/HowToUseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HowToUseViewController.swift
+//  SwimNote
+//
+//  Created by 雨宮佳音 on 2020/05/21.
+//  Copyright © 2020 kanon. All rights reserved.
+//
+
+import UIKit
+
+class HowToUseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func back(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
